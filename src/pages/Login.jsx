33b0b6e47978aad715e143af7e626704316c41bc
Chathu-Karunarathne <karunarathnechathu001@gmail.com>
2024-08-05 22:00:35 +0530

    update login and register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,34 +1,101 @@
-import React from 'react';
+import { useState, useEffect } from "react";
 import { IoMdMail } from "react-icons/io";
 import { FaLock } from "react-icons/fa";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { BiCheckbox } from "react-icons/bi";
 
+export default function Login() {
+  const initialValues = { email: "", password: "" };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
 
-const Login = () => {
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const errors = validate(formValues);
+    setFormErrors(errors);
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!values.email) {
+      errors.email = "Email is required";
+    } else if (!regex.test(values.email)) {
+      errors.email = "This is not a valid email format!";
+    }
+    if (!values.password) {
+      errors.password = "Password is required";
+    } else if (values.password.length < 4) {
+      errors.password = "Password must be more than 4 characters";
+    } else if (values.password.length > 10) {
+      errors.password = "Password cannot exceed more than 10 characters";
+    }
+
+    return errors;
+  };
+
   return (
-    <div className='h-[100vh] flex flex-col items-center bg-slate-300 justify-center text-white'>
-      <div className='h-[350px] w-80 bg-slate-400 rounded-xl px-6 my-4'>
-        <div className=''>
-          <h2 className='text-3xl font-bold pb-4 m-4 text-center'>Login</h2>
-          <form className="flex flex-col items-center" action="">
-            <div className='w-full relative'>
-              <input className="border border-gray-500 w-full rounded-full py-2 px-4 my-2 bg-transparent" placeholder="Email" type="email"/>
-              <IoMdMail className='absolute top-[35%] right-3' />
+    <div className="h-[100vh] flex flex-col items-center bg-slate-400 justify-center text-white">
+      <div className="h-[400px] w-80 bg-slate-500 rounded-xl px-6 my-4">
+        <div className="">
+          <h2 className="text-3xl font-bold pb-4 m-4 text-center">Login</h2>
+          <form className="flex flex-col items-center" onSubmit={handleSubmit}>
+            <div className="w-full relative">
+              <input
+                className="border border-gray-600 w-full rounded-full py-2 px-4 my-2 bg-transparent"
+                name="email"
+                placeholder="Email"
+                type="email"
+                value={formValues.email}
+                onChange={handleChange}
+              />
+              {formErrors.email && (
+                <p className="text-red-500">{formErrors.email}</p>
+              )}
+              <IoMdMail className="absolute top-[35%] right-3" />
             </div>
-            <div className='w-full relative'>
-              <input className="border border-gray-500 w-full rounded-full py-2 px-4 mt-2 bg-transparent" placeholder="Password" type="password"/>
-              <FaLock className='absolute top-[35%] right-3' />
+            <div className="w-full relative">
+              <input
+                className="border border-gray-600 w-full rounded-full py-2 px-4 mt-2 bg-transparent"
+                name="password"
+                placeholder="Password"
+                type="password"
+                value={formValues.password}
+                onChange={handleChange}
+              />
+              {formErrors.password && (
+                <p className="text-red-500">{formErrors.password}</p>
+              )}
+              <FaLock className="absolute top-[35%] right-3" />
             </div>
-            <button className='my-2 py-2 w-full rounded-full bg-slate-400'>Login</button>
-            <span>Don't have an account? <span className='cursor-pointer'><Link to="/register"> Register </Link>
-              </span></span>
+            
+              <div className="py-4 cursor-pointer">Forgot Password</div>
+            
+            <button className="my-2 py-2 w-full rounded-full bg-slate-500">
+              Login
+            </button>
+            <span>
+              Don't have an account?{" "}
+              <span className="cursor-pointer">
+                <Link to="/register">Register</Link>
+              </span>
+            </span>
           </form>
         </div>
       </div>
     </div>
   );
-};
-
-export default Login;
-
-
+}
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -9,7 +9,7 @@ import { Link } from "react-router-dom";
 export default function Register() {
   const initialValues = { username: "", email: "", password: "" };
   const [formValues, setFormValues] = useState(initialValues);
-  const [formErrors, setFormerrors] = useState({});
+  const [formErrors, setFormErrors] = useState({});
   const [isSubmit, setIsSubmit] = useState(false);
 
   const handleChange = (e) => {
@@ -19,122 +19,114 @@ export default function Register() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    validate(formValues);
-    setFormerrors(validate(formValues));
+    const errors = validate(formValues);
+    setFormErrors(errors);
     setIsSubmit(true);
   };
 
-  // useEffect(() => {
-  //   console.log(formValues);
-  //   if (Object.keys(formErrors).length === 0 && isSubmit) {
-  //     console.log(formValues);
-  //   }
-  // }, [formErrors]);
+  useEffect(() => {
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
 
   const validate = (values) => {
     const errors = {};
-    // const regex = "abc";
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     if (!values.username) {
-      errors.username = "username is required";
+      errors.username = "Username is required";
     }
     if (!values.email) {
-      errors.email = "email is required";
+      errors.email = "Email is required";
+    } else if (!regex.test(values.email)) {
+      errors.email = "This is not a valid email format!";
     }
-    // else if (!regex.test(values.email)) {
-    // errors.email = "This is not a valid email format!";
-    // }
     if (!values.password) {
-      errors.password = "password is required";
-    } else if (!regex.test(values.password.length < 4)) {
-      errors.password = "password must be more than 4 characters";
-    } else if (!regex.test(values.password.length > 10)) {
-      errors.email = "Password connot exceed more than 10 characters";
+      errors.password = "Password is required";
+    } else if (values.password.length < 4) {
+      errors.password = "Password must be more than 4 characters";
+    } else if (values.password.length > 10) {
+      errors.password = "Password cannot exceed more than 10 characters";
     }
 
-    return (
-      <div>
-        {/* {Object.keys(formErrors).length === 0 && isSubmit ? (
-          <div className="ui message success">Signed in successfully</div>
-        ) : (
-          <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
-        )} */}
-        <form onSubmit={handleSubmit}>
-          <div className="h-[100vh] flex  items-center bg-slate-400 justify-center text-white">
-            <div className="flex items-center gap-20  w-[95%] p-5 rounded-md overflow-hidden">
-              <img
-                className="w-[75%] h-[450px] object-cover  rounded-xl"
-                src={Registerimage}
-                alt="Register"
-              />
-              <div className="h-[400px] w-[25%]  bg-slate-500 rounded-xl  px-6 ">
-                <div className="">
-                  <h2 className="text-3xl font-bold pb-2 m-4 text-center">
-                    {" "}
-                    Register
-                  </h2>
-                  <form className="flex flex-col items-center" action="">
-                    <div className="w-full relative">
-                      <input
-                        className="border border-gray-600 w-full rounded-full py-2 px-4 my-2 bg-transparent"
-                        name="username"
-                        placeholder="Username"
-                        type="text"
-                        // value={formValues.username}
-                        // onChange={handleChange}
-                      />
-                      {/* <p>{formErrors.username}</p> */}
-                      <FaUser className="absolute top-[35%]  right-3" />
-                    </div>
-                    <div className="w-full  relative">
-                      <input
-                        className="border border-gray-600 w-full rounded-full  py-2 px-4 my-2 bg-transparent"
-                        name="email"
-                        placeholder="Email"
-                        type="email"
-                        // value={formValues.email}
-                        // onChange={handleChange}
-                      />
-                      {/* <p>{formErrors.email}</p> */}
-                      <IoMdMail className="absolute top-[35%] right-3" />
-                    </div>
-                    <div className="w-full relative">
-                      <input
-                        className="border border-gray-600 w-full rounded-full py-2 px-4 mt-2 bg-transparent"
-                        name="password"
-                        placeholder="Password"
-                        type="password"
-                        // value={formValues.password}
-                        // onChange={handleChange}
-                      />
-                      {/* <p>{formErrors.password}</p> */}
-                      <FaLock className="absolute top-[35%] right-3" />
-                    </div>
-                    <div className="w-full relative">
-                      <input
-                        className="border border-gray-600 w-full rounded-full py-2 px-4 mt-2 bg-transparent"
-                        name="confirmpassword"
-                        placeholder="ConfirmPassword"
-                        type="password"
-                      />
-                      <FaRegEyeSlash className="absolute top-[35%] right-3" />
-                    </div>
-
-                    <button className="my-2 py-2 w-full rounded-full bg-slate-500">
-                      Register
-                    </button>
-                    <span>
-                      Already have an account?{" "}
-                      <span className="cursor-pointer">
-                        <Link to="/login">Login</Link>
-                      </span>
-                    </span>
-                  </form>
-                </div>
-              </div>
-            </div>
-          </div>
-        </form>
-      </div>
-    );
+    return errors;
   };
+
+  return (
+    <div className="h-[100vh] flex items-center bg-slate-400 justify-center text-white">
+      <div className="flex items-center gap-20 w-[95%] p-5 rounded-md overflow-hidden">
+        <img
+          className="w-[75%] h-[500px] object-cover rounded-xl"
+          src={Registerimage}
+          alt="Register"
+        />
+        <div className="h-[450px] w-[25%] bg-slate-500 rounded-xl px-6">
+          <div className="">
+            <h2 className="text-3xl font-bold pb-2 m-4 text-center">Register</h2>
+            <form className="flex flex-col items-center" onSubmit={handleSubmit}>
+              <div className="w-full relative">
+                <input
+                  className="border border-gray-600 w-full rounded-full py-2 px-4 my-2 bg-transparent"
+                  name="username"
+                  placeholder="Username"
+                  type="text"
+                  value={formValues.username}
+                  onChange={handleChange}
+                />
+                <p className="text-red-500">{formErrors.username}</p>
+                <FaUser className="absolute top-[35%] right-3" />
+              </div>
+              <div className="w-full relative">
+                <input
+                  className="border border-gray-600 w-full rounded-full py-2 px-4 my-2 bg-transparent"
+                  name="email"
+                  placeholder="Email"
+                  type="email"
+                  value={formValues.email}
+                  onChange={handleChange}
+                />
+                <p className="text-red-500">{formErrors.email}</p>
+                <IoMdMail className="absolute top-[35%] right-3" />
+              </div>
+              <div className="w-full relative">
+                <input
+                  className="border border-gray-600 w-full rounded-full py-2 px-4 mt-2 bg-transparent"
+                  name="password"
+                  placeholder="Password"
+                  type="password"
+                  value={formValues.password}
+                  onChange={handleChange}
+                />
+                <p className="text-red-500">{formErrors.password}</p>
+                <FaLock className="absolute top-[35%] right-3" />
+              </div>
+              <div className="w-full relative">
+                <input
+                  className="border border-gray-600 w-full rounded-full py-2 px-4 mt-2 bg-transparent"
+                  name="confirmPassword"
+                  placeholder="Confirm Password"
+                  type="password"
+                />
+                <FaRegEyeSlash className="absolute top-[35%] right-3" />
+              </div>
+
+              <button className="my-2 py-2 w-full rounded-full bg-slate-500">
+                Register
+              </button>
+              <span>
+                Already have an account?{" "}
+                <span className="cursor-pointer">
+                  <Link to="/login">Login</Link>
+                </span>
+              </span>
+            </form>
+            {Object.keys(formErrors).length === 0 && isSubmit && (
+              <div className="ui message success px-8 py-5 font-bold">Signed in successfully</div>
+            )}
+            {/* <pre>{JSON.stringify(formValues, undefined, 2)}</pre> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
